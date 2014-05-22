f809226075b53311f32eb04eb007c712b885a373
eck32 <lbrand32@gmail.com>
2014-05-21 21:01:21 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -125,9 +125,10 @@ window.setImmediate = (function () {
       }
     }
     iterationCounter++;
+    alert(oldBitmaps[iterationCounter]);
     if(iterationCounter < 20){
       oldBitmaps[iterationCounter] = ctx.getImageData(0, 0, width, height);
-      alert(oldBitmaps[iterationCounter]);
+      //alert(oldBitmaps[iterationCounter]);
     }
     
     // Repeat immediately
