b8831d5efcdf9002f8534a8bdfc340431348e10d
eck32 <lbrand32@gmail.com>
2014-06-03 09:37:59 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -140,7 +140,6 @@ window.setImmediate = (function () {
   // Copy the  bitmap to the canvas every frame
   function draw() {
     window.requestAnimFrame(draw);
-    oldBitmaps[config.iteration] = oldBitmaps[config.iteration].toDataURL();
     ctx.putImageData(oldBitmaps[config.iteration], 0, 0);
   }
 
